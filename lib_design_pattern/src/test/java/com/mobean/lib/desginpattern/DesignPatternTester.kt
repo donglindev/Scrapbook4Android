179d591098ed30507ef941a58968653370424cad
Amit <company.donglin@gmal.com>
2022-05-15 11:06:35 +0800

    初始化设计模式lib库

---
+++ lib_design_pattern/src/test/java/com/mobean/lib/desginpattern/DesignPatternTester.kt
@@ -0,0 +1,17 @@
+package com.mobean.lib.desginpattern
+
+import org.junit.Test
+
+/**
+ * 设计模式测试类
+ *
+ * @author Amit
+ * @date 2022/05/15
+ */
+class DesignPatternTester {
+
+    @Test
+    fun testDesignPattern() {
+        println("I'm design pattern tester function.");
+    }
+}
